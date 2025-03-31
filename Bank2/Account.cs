ea16a6ce5370a014444181e94d2b59b528f7c1a2
Wolvye <alainsoltau@googlemail.com>
2025-03-31 15:33:54 +0200

    Ohne Stuetzraeder

---
+++ Bank2/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank;
+
+
+
+namespace Bank
+{
+    internal class Account
+    {
+        public string Number { get; private set; }
+        public string Owner { get; private set; }
+        public float Balance { get; private set; }
+
+        public Account(string number, string owner, float balance)
+        {
+            Number = number;
+            Owner = owner;
+            Balance = balance;
+        }
+        public void abheben(float summe)
+        {
+            Balance -= summe;
+        }
+        public void einzahlen(float summe)
+        {
+            Balance = summe + Balance;
+        }
+        public float kontostand()
+        {
+            return Balance;
+        }
+    }
+}
--- Bank2/Program.cs
+++ Bank2/Program.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Bank;
+using System;
 
 namespace Bank
 {
     internal class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Account Konto = new Account("123456", "Doe", 400);
+            Console.WriteLine("Wähle 1 für Kontostand,2 für Abheben, 3 für einzahlen oder 4 für Abbruch");
+            int command = int.Parse(Console.ReadLine());
+
+            while (command != 4)
+            {
+                if (command == 1)
+                {   
+                    Console.WriteLine($"Dein Kontostand beträgt {Konto.kontostand()}");
+                    Console.WriteLine("Wähle 1 für Kontostand,2 für Abheben, 3 für einzahlen oder 4 für Abbruch");
+                    command = int.Parse(Console.ReadLine());
+
+
+                }
+                else if (command == 2)
+                {
+
+
+                    if (Konto.Balance <= 0)
+                    {
+                        Console.WriteLine("Du hast kein Parra, Kenek!");
+                        
+                        command = int.Parse(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wie viel willst du abheben?");
+                        float abhebeSumme =float.Parse(Console.ReadLine());
+                        Konto.abheben(abhebeSumme);
+                        Console.WriteLine("Wähle 1 für Kontostand,2 für Abheben, 3 für einzahlen oder 4 für Abbruch");
+                        command = int.Parse(Console.ReadLine());
+                    }
+                }
+                else if (command == 3)
+                {
+                    Console.WriteLine("Wie viel willst du einzahlen?");
+                    float einzahlSumme=float.Parse(Console.ReadLine());
+                    Konto.einzahlen(einzahlSumme);
+                    Console.WriteLine("Wähle 1 für Kontostand,2 für Abheben, 3 für einzahlen oder 4 für Abbruch");
+                    command = int.Parse(Console.ReadLine());
+                }
+                else if (command == 4)
+                {
+                    break;
+                }
+            }
         }
     }
 }
